f4cb98f49f49f92043111a4fb45ba0c769d1bf9a
Isis Macedo <89617847+isismodd@users.noreply.github.com>
2025-09-26 16:43:19 -0300

    Update IntegrantesCard.jsx

--- sprint3/src/componentes/IntegrantesCard.jsx
+++ sprint3/src/componentes/IntegrantesCard.jsx
@@ -1,3 +1,6 @@
+import iconGit from '../img/image 2.png';
+import iconLinkedin from '../img/image 3.png';
+
 const IntegranteCard = ({ 
   foto, 
   nome, 
@@ -6,7 +9,27 @@ const IntegranteCard = ({
   git,
   linkedin
 }) => {
-  return ();
+  return (
+    <>
+    <div className='flex flex-col justify-center rounded-2xl items-center w-[333px] h-[493px] shadow-xl'>
+      <img 
+        src={foto} 
+        className='pb-5' 
+        alt={Foto Integrante} 
+      />
+      <h4 className='font-bold text-2xl text-bg text-center'>{nome}</h4>
+      <p className='text-xl text-blue-light'>{turma} - RM{rm}</p>
+      <div className='flex pt-5 w-full justify-evenly'>
+          <a href={git}>
+            <img src={iconGit}  alt="GitHub" />
+          </a>
+          <a href={linkedin}>
+            <img src={iconLinkedin} alt="LinkedIn" />
+</a>
+      </div>
+    </div>
+    </>
+  );
 };
 
 export default IntegranteCard;
