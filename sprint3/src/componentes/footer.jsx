6dc3a0160b08a9925af2de24d813728cb9da1d17
Isis Macedo <89617847+isismodd@users.noreply.github.com>
2025-09-26 16:42:49 -0300

    Update footer.jsx

--- sprint3/src/componentes/footer.jsx
+++ sprint3/src/componentes/footer.jsx
@@ -1,5 +1,35 @@
-function Footer() {
-  return ()
-}
+import iconGit from '../img/image 2.png';
+import iconLinkedin from '../img/image 3.png';
 
-export default Footer;
+const IntegranteCard = ({ 
+  foto, 
+  nome, 
+  turma = '1TDSR', 
+  rm, 
+  git,
+  linkedin
+}) => {
+  return (
+    <>
+    <div className='flex flex-col justify-center rounded-2xl items-center w-[333px] h-[493px] shadow-xl'>
+      <img 
+        src={foto} 
+        className='pb-5' 
+        alt={Foto Integrante} 
+      />
+      <h4 className='font-bold text-2xl text-bg text-center'>{nome}</h4>
+      <p className='text-xl text-blue-light'>{turma} - RM{rm}</p>
+      <div className='flex pt-5 w-full justify-evenly'>
+          <a href={git}>
+            <img src={iconGit}  alt="GitHub" />
+          </a>
+          <a href={linkedin}>
+            <img src={iconLinkedin} alt="LinkedIn" />
+</a>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default IntegranteCard;
