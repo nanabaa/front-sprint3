f5bf741c017f13d8570a9be3c198074af14b677c
nana <anaclaranasc23@gmail.com>
2025-09-28 16:30:47 -0300

    solucoes

--- sprint3/src/pages/Solucoes.jsx
+++ sprint3/src/pages/Solucoes.jsx
@@ -20,6 +20,15 @@ function Solucoes() {
             <section className='min-h-screen bg-text-white flex flex-col content-center text-justify flex-wrap lg:p-25 gap-10 '>
                     <div className='flex flex-row justify-center md:justify-between mt-20 items-center w-sm sm:w-lg  md:w-2xl lg:w-5xl xl:w-auto flex-wrap xl:flex-nowrap '>
                         <div className=' flex flex-col gap-10 w-6xl'>
+                    <h4 className='text-3xl lg:text-6xl  font-bold text-text'>Problematização</h4>
+                <p className='text-xl lg:text-2xl leading-13'>Na parceria com o HC, obtivemos a informação de que há cerca de 20% de faltas nas consultas digitais. Isso ocorre por causa do “Absenteísmo digital”, que refere-se à ausência ou falta de acesso e utilização adequada das tecnologias digitais, que ocorre tanto por fatores econômicos, como o alto custo de serviço de internet, a falta de condições para comprar um aparelho eletrônico, ou para comprar um novo que tenha o devido suporte para certas funções; assim como fatores etários. Hoje em dia nascemos com uma maior possibilidade de imersão digital, diferente das gerações passadas que não tiveram essa oportunidade e por isso podem ter dificuldades em certos recursos.</p>
+                <p className='text-xl lg:text-2xl leading-13'>De acordo com um Índice de Privação On-Line feito pela PwC, 33,9 milhões de brasileiros não estão conectados à internet, sendo a maioria deles homens, idosos, não alfabetizados e das classes C, D e E.
+Além disso, somente 20% da população brasileira que usa a internet têm acesso de qualidade à rede.</p>
+                </div>
+                </div>
+                    
+                    <div className='flex flex-row justify-center md:justify-between mt-20 items-center w-sm sm:w-lg  md:w-2xl lg:w-5xl xl:w-auto flex-wrap xl:flex-nowrap '>
+                        <div className=' flex flex-col gap-10 w-6xl'>
                 <h4 className='text-3xl lg:text-6xl  font-bold text-text'>Nossa solução</h4>
                 <p className='text-xl lg:text-2xl leading-13'>Focando em um design intuitivo e que muitos já estão familiarizados, inclusive o público sênior, propomos uma solução simples, que agrupa todos os chats usados pelo usuário em diferentes plataformas (como o Whatsapp e o email, usados pelo HC) para diferentes funções, em uma única página de chat, separando suas funções por contato e facilitando o acesso à essas informações, incluindo um novo ChatBot automatizado e os tutoriais para uso do app e para todo o processo de consulta.</p>
 
