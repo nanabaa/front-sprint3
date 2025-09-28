2b3a83ff3e00ce123709002df22f5b4943997018
nana <anaclaranasc23@gmail.com>
2025-09-28 01:16:41 -0300

    correçao contato

--- sprint3/src/pages/Contato.jsx
+++ sprint3/src/pages/Contato.jsx
@@ -7,9 +7,9 @@ function Contato() {
     const numeros = value.replace(/\D/g, '').slice(0, 11);
 
     if (numeros.length <= 2) return numeros;
-    if (numeros.length <= 6) return (${numeros.slice(0, 2)}) ${numeros.slice(2)};
-    if (numeros.length <= 10) return (${numeros.slice(0, 2)}) ${numeros.slice(2, 6)}-${numeros.slice(6)};
-    return (${numeros.slice(0, 2)}) ${numeros.slice(2, 7)}-${numeros.slice(7, 11)};
+    if (numeros.length <= 6) return `(${numeros.slice(0, 2)}) ${numeros.slice(2)}`;
+    if (numeros.length <= 10) return `(${numeros.slice(0, 2)}) ${numeros.slice(2, 6)}-${numeros.slice(6)}`;
+    return `(${numeros.slice(0, 2)}) ${numeros.slice(2, 7)}-${numeros.slice(7, 11)}`;
   };
 
   const telefoneChange = (e) => {
